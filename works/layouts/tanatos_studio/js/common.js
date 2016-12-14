d19b933731c33d9e6bb5af871740f12b17600f4c
gaskarov serega <gaskarov serega>
2016-12-14 15:48:57 +0500

    Переделаны все секции

---
+++ works/layouts/tanatos_studio/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $(".carouse").owlCarousel({
+        navigation : true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem: true,
+        navigationText: false
+    });
+
+});
