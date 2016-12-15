b66a80264d033a67c3100a820f862454cadda8f1
gaskarov serega <gaskarov serega>
2016-12-15 11:39:10 +0500

    Отредактирование дестоп версия + скрипты

--- works/layouts/tanatos_studio/js/common.js
+++ works/layouts/tanatos_studio/js/common.js
@@ -8,4 +8,23 @@ $(document).ready(function() {
         navigationText: false
     });
 
+  $(".navigation").hide();
+
+  $(".buttonBurger").click(function(){
+    $(".navigation").css({"opacity": "1"}).show();
+  });
+  $(".closed").click(function(){
+    $(".navigation").css({"opacity": "0"}).hide();
+  });
+
+  $("#nav, .arroWrap").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+       //узнаем высоту от начала страницы до блока на который ссылается якорь
+         top = $(id).offset().top;
+       //анимируем переход на расстояние - top за 1500 мс
+       $('body,html').animate({scrollTop: top+10}, 1500);
+       $("#nav").css({"opacity": "0"}).hide();
+
+   });
 });
