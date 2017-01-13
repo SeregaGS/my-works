fd57dcf179ad6b5f68d15c890fd29b78821de6da
gaskarov serega <gaskarov serega>
2017-01-13 22:40:55 +0500

    test1C2

---
+++ works/layouts/zapravkin/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#carousel').owlCarousel({
+    items     : 1,
+    loop      : true,
+    nav       : true,
+    autoPlay  : 5000,
+    navigation : true,
+    navigationText : false
+    });
+});
