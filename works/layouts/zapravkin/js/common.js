7ab30ff43277a5be84cd6877647e70e0737ffd11
gaskarov serega <gaskarov serega>
2017-01-14 14:27:29 +0500

    mapyandextest-1

--- works/layouts/zapravkin/js/common.js
+++ works/layouts/zapravkin/js/common.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
     navigation : true,
     navigationText : false
     });
+    $('#clients-carousel').owlCarousel({
+      items     : 6,
+      loop      : true,
+      nav       : false,
+      autoPlay  : 5000,
+      navigation : false,
+      navigationText : false
+      });
 });
