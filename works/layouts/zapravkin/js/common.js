2ff7eb618c801252fd8f794ea0fd6b1dc84ff7ab
gaskarov serega <gaskarov serega>
2017-01-16 22:43:11 +0500

    test3.1

--- works/layouts/zapravkin/js/common.js
+++ works/layouts/zapravkin/js/common.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
   $('#carousel').owlCarousel({
     items     : 1,
+    itemsDesktop : [1000,1],
+    itemsDesktopSmall : [900,1],
+    itemsTablet: [600,1],
+    itemsMobile : false,
     loop      : true,
     nav       : true,
     autoPlay  : 5000,
     navigation : true,
     navigationText : false
-    });
-    $('#clients-carousel').owlCarousel({
-      items     : 6,
-      loop      : true,
-      nav       : false,
-      autoPlay  : 5000,
-      navigation : false,
-      navigationText : false
-      });
+  });
+  $('#clients-carousel').owlCarousel({
+    items     : 6,
+    loop      : true,
+    nav       : false,
+    autoPlay  : 5000,
+    navigation : false,
+    navigationText : false
+  });
+
+  $('.number-link, .prices-link').magnificPopup({
+  type:'inline',
+  midClick: true
+});
 });
